f675e20eea5669fd237492b6ae7de9bb29e63ac5
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-10-09 21:56:13 +0000

    base de datos instagram

--- src/models.py
+++ src/models.py
@@ -5,33 +5,73 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
-Base = declarative_base()
+BaseInsta = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(BaseInsta):
+    __tablename__ = 'user'
+    id = Column (Integer, primary_key=True)
+    name = Column (String(250), nullable=False)
+    user_name= Column (String(30), nullable=False)
+    email= Column (String(100), nullable=False)
+    password= Column (String(100), nullable=False)
+    registration_date= Column (Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(BaseInsta):
+    __tablename__ = 'post'
+    id_post = Column (Integer, primary_key=True)
+    creation_date = Column (Integer)
+    like_post = Column (Integer)
+    user_id= Column (Integer, ForeignKey ('user.id'))
+    user = relationship ('user.id')
+
+class Video(BaseInsta):
+    __tablename__= 'video'
+    id_reels_= Column (Integer, primary_key=True)
+    reproductions = Column(Integer)
+    likes = Column (Integer)
+    comments= Column (Integer)
+    times_it_has_been_saved= Column (Integer)
+    times_it_has_been_shared= Column (Integer)
+    hashtags_used= Column (Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Reel(BaseInsta):
+    __tablename__= 'reel'
+    id_reel = Column (Integer, primary_key=True)
+    reproductions =  Column (Integer)
+    likes = Column (Integer)
+    comments = Column (Integer)
+    times_it_has_been_saved = Column(Integer)
+    times_it_has_been_shared = Column(Integer)
+    hashtags_used = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)   
+
+class Photo(BaseInsta):
+    __tablename__= 'photo'
+    id_photo = Column (Integer, primary_key=True)
+    impressions = Column (Integer) 
+    likes = Column (Integer)
+    comments = Column (Integer)
+    times_it_has_been_saved = Column (Integer)
+    times_it_has_been_shared = Column (Integer)
+    hashtags_used = Column (Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User) 
+
+class Followers (BaseInsta):
+    __tablename__ = 'followers'
+    followers = Column (Integer, primary_key=True)
+    followed = Column (Integer)
+    direct_messages = Column (Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+engine = create_engine('sqlite:///workspaces/exercise-instagram-data-modeling-LexoBrunett')
+render_er(BaseInsta, 'diagram.png')
